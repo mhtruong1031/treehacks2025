3b447e873f87df851be79c9020341fabb7378565
mhtruong1031 <mhtruong1031@gmail.com>
2025-02-14 23:11:18 -0800

    balls

--- download_data.py
+++ download_data.py
@@ -3,10 +3,9 @@ import openpyxl
 with open("mutual_ids.txt", "r") as f:
     mutual_ids = f.read().splitlines()
 
-print(mutual_ids)
 wb = openpyxl.load_workbook('genetic_data_unfiltered.xlsx')
 gd = wb["Genetic Data (unfiltered)"]
 
 for i in range(len(gd['A'])):
     if gd.cell(i+1, 1).value in mutual_ids:
-        print(gd.cell(i+1, 6).hyperlink)
+        print(gd.cell(i+1, 6).hyperlink.target)
